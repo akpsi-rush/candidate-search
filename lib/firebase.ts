06f64ef4fa062b9dd190885e414f0ed83211085a
sachink2413 <sachink2@illinois.edu>
2024-12-23 16:11:55 -0600

    switched firebase config to point towards spring 2025 and changed db route

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -7,6 +7,16 @@ import { getDatabase } from "firebase/database";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const newfirebaseConfig = {
+  apiKey: "AIzaSyAZB6tIHLi-7jpARvMmDR-iF7sPrr1wq3I",
+  authDomain: "spring-2025-rush.firebaseapp.com",
+  databaseURL: "https://spring-2025-rush-default-rtdb.firebaseio.com",
+  projectId: "spring-2025-rush",
+  storageBucket: "spring-2025-rush.firebasestorage.app",
+  messagingSenderId: "1068163791827",
+  appId: "1:1068163791827:web:d1611aaed60abb885f8d26"
+};
+
 const firebaseConfig = {
   apiKey: "AIzaSyB0mkJ86kq8EAdih5YbiPh7hzegbm5GCGA",
   authDomain: "fall-2024-rush.firebaseapp.com",
@@ -18,8 +28,9 @@ const firebaseConfig = {
   measurementId: "G-NG0W0Z2SVJ"
 };
 
+
 // Initialize Firebase
-export const app = initializeApp(firebaseConfig);
+export const app = initializeApp(newfirebaseConfig);
 export const db = getDatabase(app);
 export const auth = getAuth(app);
 
