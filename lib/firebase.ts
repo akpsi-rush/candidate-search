0b8c881f622d4f439e44b1385c0b9b8ba4cfeeb4
sachink2413 <sachink2@illinois.edu>
2025-01-26 11:38:45 -0600

    database route changed

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -7,7 +7,7 @@ import { getDatabase } from "firebase/database";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const newfirebaseConfig = {
+const firebaseConfig = {
   apiKey: "AIzaSyAZB6tIHLi-7jpARvMmDR-iF7sPrr1wq3I",
   authDomain: "spring-2025-rush.firebaseapp.com",
   databaseURL: "https://spring-2025-rush-default-rtdb.firebaseio.com",
@@ -17,20 +17,20 @@ const newfirebaseConfig = {
   appId: "1:1068163791827:web:d1611aaed60abb885f8d26"
 };
 
-const firebaseConfig = {
-  apiKey: "AIzaSyB0mkJ86kq8EAdih5YbiPh7hzegbm5GCGA",
-  authDomain: "fall-2024-rush.firebaseapp.com",
-  databaseURL: "https://fall-2024-rush-default-rtdb.firebaseio.com",
-  projectId: "fall-2024-rush",
-  storageBucket: "fall-2024-rush.appspot.com",
-  messagingSenderId: "89834741583",
-  appId: "1:89834741583:web:419ac462f7a22bd908ef3a",
-  measurementId: "G-NG0W0Z2SVJ"
-};
+// const firebaseConfig = {
+//   apiKey: "AIzaSyB0mkJ86kq8EAdih5YbiPh7hzegbm5GCGA",
+//   authDomain: "fall-2024-rush.firebaseapp.com",
+//   databaseURL: "https://fall-2024-rush-default-rtdb.firebaseio.com",
+//   projectId: "fall-2024-rush",
+//   storageBucket: "fall-2024-rush.appspot.com",
+//   messagingSenderId: "89834741583",
+//   appId: "1:89834741583:web:419ac462f7a22bd908ef3a",
+//   measurementId: "G-NG0W0Z2SVJ"
+// };
 
 
 // Initialize Firebase
-export const app = initializeApp(newfirebaseConfig);
+export const app = initializeApp(firebaseConfig);
 export const db = getDatabase(app);
 export const auth = getAuth(app);
 
